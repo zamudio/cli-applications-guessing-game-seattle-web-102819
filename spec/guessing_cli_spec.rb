3ce4e42085058c825716307b2efede186e477832
Michael Z <mziacm@gmail.com>
2019-10-15 10:25:20 +0000

    Automatically backed up by Learn

--- spec/guessing_cli_spec.rb
+++ spec/guessing_cli_spec.rb
@@ -19,7 +19,7 @@ describe "Guessing CLI" do
     it "responds to a correct guess" do
       allow(self).to receive(:rand).and_return(1)
 
-      expect(self).to receive(:gets).and_return("2")
+      expect(self).to receive(:gets).and_return("1")
 
       expect { run_guessing_game }.to output(/You guessed the correct number!/).to_stdout
     end
